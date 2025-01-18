59cff008a65f822afca0d2ccb1dc7d5e499deac0
abui <adjiem31@gmail.com>
2025-01-18 23:34:41 +0700

    first kak

---
+++ script.js
@@ -0,0 +1,21 @@
+function addTask() {
+    const input = document.getElementById("tf-input").value;
+    const task = document.createElement("li");
+    task.textContent = input;
+    task.id = new Date().valueOf().toString() + Math.random().toString(36).substring(2, 7);
+    task.classList.add("list-item");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-button");
+    deleteButton.addEventListener("click", () => {
+        deleteTask(task.id);
+    });
+    task.appendChild(deleteButton);
+    document.getElementById("task-container").appendChild(task);
+    document.getElementById("tf-input").value = "";
+}
+
+function deleteTask(id) {
+    const task = document.getElementById(id);
+    task.remove();
+}
